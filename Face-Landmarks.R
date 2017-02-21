7985d0c840b3185ab82069ab2dc5bb38a989b75f
stephanieovcharik <stephanieovcharik@gmail.com>
2017-02-20 20:33:06 -0500

    Updated

--- Face-Landmarks.R
+++ Face-Landmarks.R
@@ -3,31 +3,26 @@ library("readxl")
 file.list <- list.files(pattern='*.xlsx')
 length <- length(file.list)
 for(i in seq(1, length, 4)){
-
-	person1 <- read_excel(file.list[i])
-	person2 <- read_excel(file.list[i+1])
-	person3 <- read_excel(file.list[i+2])
-	person4 <- read_excel(file.list[i+3])
-	sdX <- c()
-	sdY <- c()
-	missing <- c()
-	pointNumber <- c(1:31)
-	for(num in 1:31){
-		pointX <- c(person1[num,3],person2[num,3],person3[num,3],person4[num,3])
-		pointY <- c(person1[num,4],person2[num,4],person3[num,4],person4[num,4])
-		sdX[num] <- sd(pointX, na.rm=TRUE)
-		sdY[num] <- sd(pointY, na.rm=TRUE)
-		missing[num] <- sum(is.na(pointX),is.na(pointY))
-	}
-	finalTable <- data.frame(
-	pointNumber,
-	sdX,
-	sdY,
-	missing
-	)
-	finalTable[] <- lapply(finalTable,round,5)
-	fileName <- substring(file.list[i],1,16)
-	write.table(finalTable, paste(fileName, ".txt"))
+  
+  person1 <- read_excel(file.list[i])
+  person2 <- read_excel(file.list[i+1])
+  person3 <- read_excel(file.list[i+2])
+  person4 <- read_excel(file.list[i+3])
+  
+  library(data.table)
+  
+  xCoordinate <- data.table(x1=person1[,3],x2=person2[,3],x3=person3[,3],x4=person4[,3])
+ 
+  yCoordinate <- data.table(y1=person1[,4],y2=person2[,4],y3=person3[,4],y4=person4[,4])
+  
+  sdX <- apply(xCoordinate[,c("x1","x2","x3","x4")],1,FUN=sd,na.rm=TRUE)
+  sdY <- apply(yCoordinate[,c("y1","y2","y3","y4")],1,FUN=sd,na.rm=TRUE)
+  missing <- rowSums(is.na(xCoordinate))+ rowSums(is.na(yCoordinate))
+  
+  finalTable <- data.table(pointNumber=(1:31), sdX=sdX, sdY=sdY, missing=missing)
+  
+  fileName <- substring(file.list[i],1,16)
+  write.table(finalTable,paste(fileName, ".txt"))
 }
 
 

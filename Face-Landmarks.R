96d0b3afc15d5d88aed6abe40b4517076996e649
stephanieovcharik <stephanieovcharik@gmail.com>
2017-02-25 14:51:40 -0500

    Updated

--- Face-Landmarks.R
+++ Face-Landmarks.R
@@ -1,28 +1,35 @@
 library("readxl")
-#all files in the current working directory with .xlsx will be stored in file.list
-file.list <- list.files(pattern='*.xlsx')
-length <- length(file.list)
-for(i in seq(1, length, 4)){
-  
-  person1 <- read_excel(file.list[i])
-  person2 <- read_excel(file.list[i+1])
-  person3 <- read_excel(file.list[i+2])
-  person4 <- read_excel(file.list[i+3])
-  
-  library(data.table)
-  
-  xCoordinate <- data.table(x1=person1[,3],x2=person2[,3],x3=person3[,3],x4=person4[,3])
- 
-  yCoordinate <- data.table(y1=person1[,4],y2=person2[,4],y3=person3[,4],y4=person4[,4])
-  
-  sdX <- apply(xCoordinate[,c("x1","x2","x3","x4")],1,FUN=sd,na.rm=TRUE)
-  sdY <- apply(yCoordinate[,c("y1","y2","y3","y4")],1,FUN=sd,na.rm=TRUE)
-  missing <- rowSums(is.na(xCoordinate))+ rowSums(is.na(yCoordinate))
-  
-  finalTable <- data.table(pointNumber=(1:31), sdX=sdX, sdY=sdY, missing=missing)
-  
-  fileName <- substring(file.list[i],1,16)
-  write.table(finalTable,paste(fileName, ".txt"))
+#all files that contain ".xlsx" will be stored in file.list
+
+data.dir <- paste(data.dir,"*xlsx")
+file.list <- list.files(data.dir)
+
+
+landmarkTable <-NULL
+
+for(file in file.list){
+  x <- read_excel(file)
+  landmarkTable <- rbind(landmarkTable,x)
 }
 
+library(data.table)
+
+#convert landmarks into data table
+landmarkTable <- data.table(landmarkTable)
+
+# find standard deviation for each point's X coordinate for each face
+standardX <- landmarkTable[,sd(X,na.rm=TRUE),by=.(PointNumber,Face)]
+setnames (standardX,"V1","sdX")
+
+# find standard deviation for each point's Y coordinate for each face
+standardY <- landmarkTable[,sd(Y,na.rm=TRUE), by=.(PointNumber,Face)]
+setnames (standardY,"V1","sdY")
+
+
+missing <- landmarkTable[ , sum(is.na(X),is.na(Y)),by=.(PointNumber,Face)]
+setnames (missing,"V1","Missing")
+
+finalTable <- cbind(standardX[,1:3],standardY[,3], missing[,3])
+
+write.table(finalTable, "landmark.txt")
 
